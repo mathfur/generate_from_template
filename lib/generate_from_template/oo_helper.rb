8716171133fc6317d968bd3124ab4f1220ee257c
mathfuru <mathfuru@gmail.com>
2012-03-11 11:19:47 +0900

    全体的に書き直し

--- lib/generate_from_template/oo_helper.rb
+++ lib/generate_from_template/oo_helper.rb
@@ -27,38 +27,99 @@ module OOHelper
       @model = model
       @model_info = model_info
 
-      (@model_info.keys - self.methods).each do |name|
+      # attrにある属性にはa.fooのようにアクセスできるようにする
+      ((@model_info["attrs"] || {}).keys - self.methods).each do |name_|
+        p model if name_ == "name"
+        self.class.class_eval do
+          define_method name_ do |*args|
+            p ">>>>>"
+            p @model_info["attrs"].keys
+            p "<<<<"
+            (@model_info["attrs"] || {})[name_]["etc"]
+          end
+        end
+      end
+
+      # cttrへもアクセサ定義
+      ((@model_info["cattrs"] || {}).keys - self.methods).each do |name|
         self.class.class_eval do
           define_method name do |*args|
-            @model_info[name]
+            (((@model_info["cattrs"] || {})[name] || {})["etc"] || {})["cattr_val"]
           end
         end
       end
     end
 
-    def camelize
-      @model.camelize
+    # m.table_columns("index"){|c| .. }でindexのカラム全体を渡る
+    # options:
+    #   :blank => true <=> セルの値がブランクのものも出力する
+    def table_columns(tbl_name, options ={})
+      # {カラム名 => セルの値}ハッシュを作る
+      column_cell_hash = (@model_info["attrs"] ||{}).merge((@model_info["columns"] ||{})).map{|attr, hash| [attr, hash["tables"][tbl_name]]}.
+        select{|k, v| v.present? || options[:blank]}
+
+      if block_given?
+        column_cell_hash.each do |attr, cell_val|
+          yield attr, cell_val
+        end
+      else
+        Hash[*column_cell_hash.flatten]
+      end
     end
 
-    def table_name
-      @model.pluralize.underscore
+    def camelize; @model.camelize; end
+    def table_name; @model.pluralize.underscore; end
+    def pluralize; @model.pluralize; end
+    def singularize; @model.singularize; end
+    def underscore; @model.underscore; end
+
+    def single
+      self.singularize.underscore
+    end
+
+    def multiple
+      self.pluralize.underscore
+    end
+
+    def instance
+      self.singularize.underscore
     end
 
     alias :tbl :table_name
 
     def model_name
-      @model.pluralize.camelize
+      @model.singularize.camelize
     end
 
+    def controller_name
+      @model.pluralize + "_controller"
+    end
+
+    alias :model :model_name
+    alias :table :table_name
+
+
     def attrs(&block)
-      @model_info["attrs"].each do |attr, attr_info|
-        attr = Attr.new(attr, attr_info)
-        block.call(attr)
+      if block_given?
+        (@model_info["attrs"] ||{}).each do |attr, attr_info|
+          attr = Attr.new(attr, attr_info["etc"])
+          block.call(attr)
+        end
+      else
+        Hash[*@model_info["attrs"].map{|attr, attr_info| [attr, attr_info["etc"]]}.flatten]
       end
     end
 
     def attr_names
-      self.attr.keys
+      self.attrs.keys
+    end
+
+    def has_many?(m2)
+      !!m2.attr_names.find{|name| name == "#{self.singularize.underscore}_id" }
+    end
+
+    def belongs_to?(m2)
+      m2.has_many?(self)
     end
   end
 
@@ -72,7 +133,14 @@ module OOHelper
       @attr_info.keys.each do |name|
         self.class.class_eval do
           define_method name do |*args|
-            @attr_info[name]
+            if 2 <= args.length
+              # a.default "null" ":default => $v"のように書くと、
+              # defaultの値が"null"の時は空文字列、その他の場合は":default => #{@attr_info[name]}"を返す
+              value_is_null, value_is_not_null = args
+              @attr_info[name] == value_is_null ? "" : value_is_not_null.gsub('$v', @attr_info[name]).gsub(/\bTRUE\b/, "true").gsub(/\bFALSE\b/, "false")
+            else
+              @attr_info[name]
+            end
           end
         end
       end
--- lib/generate_from_template/template.rb
+++ lib/generate_from_template/template.rb
@@ -2,10 +2,15 @@
 
 require "erb"
 require "fileutils"
+require File.dirname(__FILE__) + "/oo_helper"
 
 class Template
-  def initialize(fname, csv_fname)
-    @templates = ERB.new(File.read(fname), nil, '-').result(binding).scan(/--\[([^\:]*):([^\]:]*)\]-{10,}\n(.*?)\n--\n/m)
+  attr_reader :erb_result
+
+  def initialize(fname, csv_fname, options = {})
+    @erb_result = ERB.new(File.read(fname), nil, '-').result(binding)
+    @templates = @erb_result.scan(/\n--\[([^\:]+):([^\]:]+)\]-{10,}(?:\n|\n(.*?)\n)--\n/m)
+    @verbose = options[:verbose]
   end
 
   def each_at(base, &block)
@@ -15,9 +20,10 @@ class Template
       result = block.call(full_path, actor, str, templ)
       unless Dir.exist?(File.dirname(full_path))
         FileUtils.mkdir_p(File.dirname(full_path))
-        STDERR.puts "directory '#{File.dirname(full_path)}' was created."
+        STDERR.puts "directory '#{File.dirname(full_path)}' was created." if @verbose
       end
       open(full_path,'w'){|f| f.write result}
+      STDERR.puts "write to #{full_path}" if @verbose
     end
   end
 end
